4a0bcb9159098d1664118a00fa151c20460bfd0d
Siva Prasad <sivaprasad@ayatas.com>
2015-02-05 18:11:50 +0530

    changes

--- application/views/landing.php
+++ application/views/landing.php
@@ -40,6 +40,12 @@
     <div class="col-md-6 col-sm-6 col-xs-12 rightCol pull-right">
       <div class="signupToday">
         <h5>Sign Up Today!</h5>
+        <span style="color:#F00; padding:0 0 0 16px">
+        	<?php if($this->session->flashdata('success')){ 
+				echo $this->session->flashdata('success');
+			}?>
+            
+        </span>
         <?php
   			
   		    echo validation_errors();
@@ -70,7 +76,7 @@
             <label>Birthday</label>
             <div data-date-viewmode="years" data-date-format="dd-mm-yyyy" data-date="12-02-2012" id="dpYears" 
             class="input-group-bt date">
-              <input type="text" name="birthdate" value="12-02-2012" size="16" class="form-control">
+              <input type="text" readonly name="birthdate" value="12-02-2012" size="16" class="form-control">
               <span aria-hidden="true" class="add-on glyphicon glyphicon-calendar"></span> </div>
           </div>
           <p>Why do I need to provide my birthday?</p>
@@ -82,9 +88,17 @@
               <input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="female" >
               Female </label>
           </div>
-          <p>By clicking Sign Up, you agree to our Terms and that you have read our Data Use Policy</p>
+          <p>Lorem ipsum dolor sit amet, at choro omnium partiendo qui, nec nulla voluptua ex, te homero dissentiunt usu. Et vis latine epicuri voluptaria, <a href="#">posse veniam legimus eu ius</a>. Odio albucius ne vis, nec ad scaevola philosophia. Vide nominavi</p>
           <div class="sbButtons">
             <input type="submit" value="Sign Up Now">
+            <br>
+            <br>
+            <div
+ 				 class="fb-like"
+ 				 data-share="true"
+ 				 data-width="450"
+  				 data-show-faces="true">
+			</div>
           </div>
         </form>
       </div>
@@ -93,6 +107,23 @@
   </div>
 </section>
 <footer>Bzzbook &copy; 2015 English (US)</footer>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '941215722569031',
+      xfbml      : true,
+      version    : 'v2.2'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=941144209242849&version=v2.0";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
 <script src="<?php echo base_url(); ?>js/jquery-1.11.1.min.js"></script> 
 <!-- Include all compiled plugins (below), or include individual files as needed --> 
