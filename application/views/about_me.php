befa00dda121d01d556604a19a2fc01df711da2e
Vijay Satyamasetti <vijaykumar@ayatas.com>
2015-04-09 18:13:18 +0530

    worked on about me page

--- application/views/about_me.php
+++ application/views/about_me.php
@@ -1,25 +1,17 @@
- <?php  $image = $this->profile_set->get_profile_pic();
-			         $data = $this->profile_set->get_userinfo(); ?>
 <section class="col-lg-9 col-md-9 nopad">
       <div class="col-xs-12 ProfileView">
         <section class="visitorBox">
- <?php $attr = array('id' => 'upload_pfpic', 'name' => 'upload_file'); ?> 
-              <?php echo form_open_multipart('profile/do_upload',$attr);?>
           <div class="visitiBoxInner">
             <figure class="compCover"><img alt="" src="<?php echo base_url(); ?>images/about_banner.jpg" class="img-responsive"></figure>
               <?php  $image = $this->profile_set->get_profile_pic();
 			         $data = $this->profile_set->get_userinfo(); ?>
             <div class="profileLogo">
-              <figure class="cmplogo"><img src="<?php echo base_url();?>uploads/<?php echo $image[0]->user_img_thumb ?>" alt="<?php echo base_url();?>uploads/<?php echo $image[0]->user_img_thumb ?>"></figure>
+              <figure class="cmplogo"><a href="#"><span class="glyphicon glyphicon-camera change-photo fileinput-button" aria-hidden="true"><em>Change Picture</em> <input type="file" name="userfile" id="userfile" size="20" required/></span></a><img src="<?php echo base_url();?>uploads/<?php echo $image[0]->user_img_thumb ?>" alt="<?php echo base_url();?>uploads/<?php echo $image[0]->user_img_thumb ?>"></figure>
               <!-- <span class="inside glyphicon glyphicon-camera" ></span>--> 
             </div>
             <h4 class="profile-name"><?php echo $data[0]['user_firstname']." ".$data[0]['user_lastname'] ?></h4>
-  <a href="#"><span class="glyphicon glyphicon-camera change-photo fileinput-button" aria-hidden="true"><em>Change Picture</em> <input type="file" name="userfile" id="pfpic" size="20" required/></span></a>
-            
-	    <div class="ProfileViewNav"></div>
-
+            <div class="ProfileViewNav"></div>
           </div>
-    </form>
         </section>
       </div>
       <section class="about-user-details">
