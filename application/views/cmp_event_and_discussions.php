a977a95c348c50bcb7355037b677d31d41a62237
sivaprasad1 <sivaprasad@ayatas.com>
2015-04-17 15:04:31 +0530

    added events and cmpinfo

---
+++ application/views/cmp_event_and_discussions.php
@@ -0,0 +1,78 @@
+    <section class="events col-lg-9 col-md-9 nopad">
+      <section class="about-user-details">
+        <h4><span aria-hidden="true" class="glyphicon glyphicon-calendar"></span> Events
+          <div class="right_event">
+            <div class="btn2 btn-black">Add Event</div>
+          </div>
+        </h4>
+        <div class="events" >
+          <section class="events col-lg-12 col-md-12 col-sm-5 col-xs-12 coloumn2 aboutme">
+           <ul>
+            <?php if(!empty($event_info)) { ?>
+              <li> <img src="<?php echo base_url(); ?>uploads/<?php if(!empty($event_info[0]['event_image'])) echo $event_info[0]['event_image']; else echo "event1.jpg"; ?>" alt="">
+                <h3><span><?php echo $event_info[0]['event_name']; ?></span></h3>
+                <div class="dates"><span class="glyphicon glyphicon-calendar"> </span><?php $unixTimestamp = strtotime($event_info[0]['event_date']);
+				 echo date('d',$unixTimestamp).", ".date('F',$unixTimestamp).", ".date('Y',$unixTimestamp); ?></div>
+                 <div class="dates"><span class="glyphicon glyphicon-map-marker"> </span><?php echo $event_info[0]['event_location']; ?></div>
+                <div class="dates"><span class="glyphicon glyphicon-heart"> </span>Hits: 09 </div>
+                
+                <div class="clearfix"></div>
+                <p><?php echo $event_info[0]['event_description']; ?></p>
+                
+                <div class="clearfix"></div>
+              </li>
+              <?php } else echo "No Events Created By This Company!..";?>
+            </ul>
+             </section>
+          </div>
+      </section>
+        <section class="about-user-details">
+         <div class="newcomments">
+              <h3><img src="<?php echo base_url(); ?>images/comment.png" alt="">Comments</h3>
+              <div class="commentboxes">
+              <ul>
+                <?php $event_discussions = $this->eventmodel->get_event_discussion($event_info[0]['event_id']); 
+				
+				if(!empty($event_discussions))
+				{
+					foreach($event_discussions as $discussion)
+					{
+						$user_info = $this->eventmodel->getuserdetails($discussion['discussion_by']);
+					
+						
+						
+				?>
+                 <li>
+                  <div class="comment_imgbox"><img src="<?php echo base_url(); ?>uploads/<?php echo $user_info[0]['user_img_thumb'] ?>" alt="<?php echo base_url(); ?>uploads/<?php echo 'default_profile_pic.png'; ?> "></div>
+                  <div class="comment_text">
+                    <h5><span class="headers"><?php echo $user_info[0]['user_firstname'] ." " .$user_info[0]['user_lastname'] ?><small><span aria-hidden="true" class="glyphicon glyphicon-time">
+                    </span>
+                    <?php 
+					$hrs = $this->customermodel->get_time_difference_php($discussion['discussion_time']);
+					echo $hrs;
+					?> 
+                    
+                  </small></span></h5>
+                    <p><?php echo $discussion['discussion_content']; ?></p>
+                  </div>
+                </li>
+                <?php } } ?>
+              </ul>
+                  <div class="newcomments">
+              <h3><img src="<?php echo base_url(); ?>images/single_comment.png" alt="">Leave a Reply</h3>
+      <form class="coment" id="event_discussion" method="post" action="<?php echo base_url(); ?>events/write_discussion/<?php echo $event_info[0]['event_id']; ?>/<?php echo $event_info[0]['event_cr_cmp']; ?>">
+              <textarea placeholder="Comment" rows="4" class="form-control" name="discussion_content"></textarea>
+              <!--<input type="hidden" name="cmp_id" value="<?php // echo $this->uri->segment(4,0);?>" />-->
+               <div class="postes">
+         		 <button type="submit" class="btn2 btn-yellow">Post Comment</button>
+              </div>
+              </form>
+             
+              </div>
+             
+            </div>
+            </div>
+          </section>
+          <div class="clearfix"></div>
+    </section>
+
---
+++ application/views/company_info.php
@@ -0,0 +1,59 @@
+<section class="col-lg-9 col-md-9 nopad">
+      <section class="about-user-details">
+        <h4><span aria-hidden="true" class="glyphicon glyphicon-briefcase"></span>About Us</h4>
+        <div class="about-user-details-inner" >
+          <section class="col-lg-12 col-md-12 col-sm-5 col-xs-12 coloumn2 aboutme">
+            <div class="posts">
+              <div class="col-md-4">
+                <ul class="nav-tabs" role="tablist" id="myTab">
+                  <li role="presentation" class="active"><a href="#proffesional" aria-controls="home" role="tab" data-toggle="tab">Company Information</a></li>
+                </ul>
+              </div>
+              <div class="tab-content col-md-8">
+                <div role="tabpanel" class="tab-pane active" id="proffesional">
+                  <div class="iner_business col-lg-12">
+                    <ul>
+                     <?php if(!empty($company_info)) { ?>
+                      <li>
+                        <div class="iner_business"></div>
+                        <div class="inner_rights">
+                          <h3>Company Name</h3>
+                          <p><?php if($company_info[0]['cmp_name']) echo ucfirst($company_info[0]['cmp_name']); else echo "Information Not Available"; ?></p>
+                        </div>
+                        <div class="clearfix"></div>
+                      </li>
+                      <li>
+                        <div class="inner_rights">
+                          <h3>Established Since</h3>
+                          <p><?php $unixTimestamp = strtotime($company_info[0]['cmp_estb']); echo date('F',$unixTimestamp).", ".date('Y',$unixTimestamp); ?></p>
+                        </div>
+                        <div class="clearfix"></div>
+                      </li>
+                      <li>
+                        <div class="inner_rights">
+                          <h3>About Company</h3>
+                          <p><?php if($company_info[0]['cmp_about']) echo $company_info[0]['cmp_about']; else echo "Information Not Available"; ?></p>
+                        </div>
+                        <div class="clearfix"></div>
+                      </li><li>
+                        <div class="inner_rights">
+                          <h3>Contact Information</h3>
+                          <p><h3>Email:</h3><?php if($company_info[0]['company_email']) echo $company_info[0]['company_email']; else echo "Information Not Available";?></p>
+                           <p><h3>Phone:</h3><?php if($company_info[0]['company_phone']) echo $company_info[0]['company_phone']; else echo "Information Not Available";?></p>
+                            <p><h3>Office:</h3><?php if($company_info[0]['company_office']) echo $company_info[0]['company_office']; else echo "Information Not Available"; ?></p>
+                             <p><h3>Fax:</h3><?php if($company_info[0]['company_fax']) echo $company_info[0]['company_fax']; else echo "Information Not Available"; ?></p>
+                        </div>
+                        <div class="clearfix"></div>
+                      </li>
+                       <?php } else echo "No Information Found"; ?>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="clear"></div>
+            </div>
+          </section>
+          <div class="clearfix"></div>
+        </div>
+      </section>
+    </section>
