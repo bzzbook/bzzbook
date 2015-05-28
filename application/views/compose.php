5e5491f1fba46c4dd8f68ee6f0746510c2c20187
Vijay Satyamasetti <vijaykumar@ayatas.com>
2015-05-28 11:21:37 +0530

    minor changes in compose page and messages view

--- application/views/compose.php
+++ application/views/compose.php
@@ -14,20 +14,20 @@
             <p style="color:green; padding-left:78px;"><?php if($this->session->flashdata('send_msg'))
 			 echo $this->session->flashdata('send_msg'); ?></p>
           
-              <div class="bott_replys col-md-5 col-md-offset-1">
+              <div class="bott_replys col-md-5">
                         <form action="<?php echo base_url(); ?>message/send_msg" method="post" enctype="multipart/form-data"> 
                         <div class="adres_box"><label style="float:left; font-weight:normal">To</label><div id="selectedfriends" style="display:block; border:none; padding-left:25px;"><div id="search_frnd_wrapper"><input type="text" name="txtsearch" id="searchfriends" onkeyup="keyupevent();" /><input type="hidden" id="addedusers" name="addedusers" /><div id="autosuggest"></div></div></div></div>
                        
                        
                       </div>
-                       <div class="bott_replys col-md-5 col-md-offset-1" style="border: 1px solid #d7d7d7;"><input style="border:none;" type="text" name="subject" placeholder="Subject" /></div>
-                      <div class="bott_replys ad_box col-md-5 col-md-offset-1">
+                       <div class="bott_replys col-md-5 " style="border: 1px solid #d7d7d7;"><input style="border:none;" type="text" name="subject" placeholder="Subject" /></div>
+                      <div class="bott_replys ad_box col-md-5 ">
                       <textarea name="message_content" name="message_content" style="  background-color: transparent;
   resize: none; border:none;"  ></textarea>
                       <div id="uploadPhotosdvPreview"></div>
 		<input id="upload" name="upload[]" type="file" multiple style="display:none;" onchange="getFiles();">
                       </div>
-                      <div class="bott_replys bott_boxcolor col-md-5 col-md-offset-1">
+                      <div class="bott_replys bott_boxcolor col-md-5 ">
                         <div class="col-md-1">
                         <button class="btn3 btn-yellow ad_ing" type="submit" name="submit" id="submit" >Sent</button>
                         </div>
--- application/views/messages_view.php
+++ application/views/messages_view.php
@@ -73,21 +73,21 @@
             <p style="color:green; padding-left:78px;"><?php if($this->session->flashdata('send_msg'))
 			 echo $this->session->flashdata('send_msg'); ?></p>
           
-              <div class="bott_replys col-md-5 col-md-offset-1">
+              <div class="bott_replys col-md-5 ">
                         <form action="<?php echo base_url(); ?>message/send_msg/<?php echo $messages['message'][0]['msg_id']; ?>" method="post" enctype="multipart/form-data"> 
                         <div class="adres_box"><label style="float:left; font-weight:normal">To</label><div id="selectedfriends" style="display:block; border:none; padding-left:25px;"><div id="search_frnd_wrapper"><input type="text" name="txtsearch" id="searchfriends" onkeyup="keyupevent();" /><input type="hidden" id="addedusers" name="addedusers" /><div id="autosuggest"></div></div></div></div>
                        
                        
                       </div>
                        
-                      <div class="bott_replys ad_box col-md-5 col-md-offset-1">
+                      <div class="bott_replys ad_box col-md-5 ">
                       <textarea id="message_content" name="message_content" style="  background-color: transparent;
   resize: none; border:none;"  ></textarea>
  
                       <div id="uploadPhotosdvPreview"></div>
 		<input onchange="getFiles();" id="upload" name="upload[]" type="file" multiple style="display:none;">
                       </div>
-                      <div class="bott_replys bott_boxcolor col-md-5 col-md-offset-1">
+                      <div class="bott_replys bott_boxcolor col-md-5 ">
                         <div class="col-md-1">
                          <input type="hidden" name="subject" id="subject" value="<?php echo $messages['message'][0]['subject']; ?>" />
                          <input type="hidden" name="removefiles" id="removefiles" value="" />
